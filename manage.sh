f04359084b3f2fdefce0a175e2595f32b0baeafa
Wissmo <Stick1234@hotmail.de>
2019-08-30 23:14:59 +0200

    Update manage.sh

--- manage.sh
+++ manage.sh
@@ -5,10 +5,11 @@ if [[ $EUID -ne 0 ]]; then
     exit
 else
 	os=$(whiptail --title "What do you want to do?" --menu "Choose an option" 16 100 9 \
-	"1)" "Start"   \
-	"2)" "Stop"   \
-	"3)" "Modify / Change session token"        \
-	"4)" "Remove"		3>&2 2>&1 1>&3
+	"1)" "Start" \
+	"2)" "Stop" \
+	"3)" "Modify session amount" \
+	"4)" "Change session token" \
+	"5)" "Remove" 3>&2 2>&1 1>&3
 	)
 	case $os in
 		"1)")
@@ -21,32 +22,16 @@ else
 			echo "Sessions has been terminated"
 			;;
 		"3)")
-			rm /root/9HitsViewer_x64/sessions/156288217488*
-			token=$(whiptail --inputbox "Enter your TOKEN" 8 78 --title "TOKEN" 3>&1 1>&2 2>&3)
-	        tokenstatus=$?
-	        if [ $tokenstatus = 0 ]; then
-	          	echo "Token has been updated"
-	        else
-	           	echo "User selected cancel"
-	           	exit
-	        fi
 			option=$(whiptail --title "How many sessions you want?" --menu "Choose an option" 16 100 9 \
-	        "1)" "Use one session"   \
-	        "2)" "Automatic max session based on system"   \
-	        "3)" "Use number you want"  3>&2 2>&1 1>&3
+	        "1)" "Use one session" \
+	        "2)" "Automatic max session based on system specs" \
+	        "3)" "Use custom number" 3>&2 2>&1 1>&3
 	        )
 	        case $option in
 	            "1)")
 	                number=1
 	                ;;
 	            "2)")
-	                export NEWT_COLORS='
-	                window=,red
-	                border=white,red
-	                textbox=white,red
-	                button=black,white
-	                '
-	                whiptail --title "WARNING" --msgbox "THIS CAN GET A YELLOW/RED FACE || RECOMMENDED USE A SINGLE SESSION" 8 78
 	                cores=`nproc --all`
 	                memphy=`grep MemTotal /proc/meminfo | awk '{print $2}'`
 	                memswap=`grep SwapTotal /proc/meminfo | awk '{print $2}'`
@@ -57,16 +42,30 @@ else
 	                if [[ $sscorelimit -le $ssmemlimit ]]
 	                then
 	                    number=$sscorelimit
+						echo "Amount of $number sessions has been set"
 	                else
                     	number=$ssmemlimit
+						echo "Amount of $number sessions has been set"
 	                fi
 	                ;;
 	            "3)")
-	                whiptail --title "WARNING" --msgbox "IF YOU SET EXCESIVE AMOUNT OF SESSIONS THIS SESSIONS MAY BE BLOCKED || RECOMMENDED USE A SINGLE SESSION" 8 78
-	                number=$(whiptail --inputbox "ENTER NUMBER OF SESSIONS" 8 78 --title "SESSIONS" 3>&1 1>&2 2>&3)
+					export NEWT_COLORS='
+	                window=,red
+	                border=white,red
+	                textbox=white,red
+	                button=black,white
+	                '
+	                whiptail --title "WARNING" --msgbox "IF YOU SET EXCESIVE AMOUNT OF SESSIONS, SESSIONS MAY BE BLOCKED || RECOMMENDED USE A AUTOMATIC SESSION" 8 78
+	                export NEWT_COLORS='
+	                window=,white
+	                border=black,white
+	                textbox=black,white
+	                button=black,white
+	                '
+					number=$(whiptail --inputbox "ENTER NUMBER OF SESSIONS" 8 78 --title "SESSIONS" 3>&1 1>&2 2>&3)
 	                numberstatus=$?
 	                if [ $numberstatus = 0 ]; then
-	                    echo "Selected amount of ${number} sessions has been set"
+	                    echo "Selected amount of $number sessions has been set"
 	                else
 	                    echo "User selected Cancel"
 	                    exit
@@ -94,6 +93,17 @@ EOFSS
 	        done
 			;;
 		"4)")
+			rm /root/9HitsViewer_x64/sessions/156288217488*
+			token=$(whiptail --inputbox "Enter your TOKEN" 8 78 --title "TOKEN" 3>&1 1>&2 2>&3)
+	        tokenstatus=$?
+	        if [ $tokenstatus = 0 ]; then
+	          	echo "Token has been updated to $token"
+	        else
+	           	echo "User selected cancel"
+	           	exit
+	        fi
+			;;
+		"5)")
 			crontab -r
 			/root/kill.sh
 			rm -R 9Hits-AutoInstall 9HitsViewer_x64 9hviewer-linux-x64.tar.bz2 crashdetect.sh crontab install.sh kill.sh manage.sh reboot.sh
