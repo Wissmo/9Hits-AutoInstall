0cab98ebfe6261e2b73145ca2e1f2d3272c04960
Wissmo <Stick1234@hotmail.de>
2019-08-30 23:33:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,11 +28,10 @@ else
         number=1
     else
         os=$(whiptail --title "What Linux Distro do you have?" --menu "Choose an option" 16 100 9 \
-        "1)" "Ubuntu"   \
-        "2)" "Debian"   \
-        "3)" "CentOS"   \
-        "4)" "Any of that"      \
-        "5)" "Dont know"        3>&2 2>&1 1>&3
+        "1)" "Ubuntu" \
+        "2)" "Debian" \
+        "3)" "CentOS" \
+        "4)" "Dont know" 3>&2 2>&1 1>&3
         )
         case $os in
             "1)")
@@ -45,80 +44,79 @@ else
                 os=3
                 ;;
             "4)")
-                echo "Sorry, for the moment this script does not support your Distro"
-                exit
-                ;;
-            "5)")
                 echo "Trying detect and install automatic"
                 os=`awk -F= '/^NAME/{print $2}' /etc/os-release`
                 if [ $os == '"Ubuntu"' ]; then
                     os=1
+				if [ $os == '"Debian"' ]; then
+					os=2
                 else
                     os=3
                 fi
         esac
         token=$(whiptail --inputbox "Enter your TOKEN" 8 78 --title "TOKEN" 3>&1 1>&2 2>&3)
-        tokenstatus=$?
-        if [ $tokenstatus = 0 ]; then
-                echo "All right"
-        else
-                echo "User selected Cancel"
-                exit
-        fi
-        option=$(whiptail --title "How much sessions you want" --menu "Choose an option" 16 100 9 \
-        "1)" "Use one session"   \
-        "2)" "Automatic max session based on system"   \
-        "3)" "Use number you want"  3>&2 2>&1 1>&3
-        )
-        case $option in
-            "1)")
-                    number=1
-                    ;;
-            "2)")
-                    export NEWT_COLORS='
-                    window=,red
-                    border=white,red
-                    textbox=white,red
-                    button=black,white
-                    '
-                    whiptail --title "WARNING" --msgbox "THIS CAN GET A YELLOW/RED FACE || RECOMMENDED USE A SINGLE SESSION" 8 78
-                    cores=`nproc --all`
-                    memphy=`grep MemTotal /proc/meminfo | awk '{print $2}'`
-                    memswap=`grep SwapTotal /proc/meminfo | awk '{print $2}'`
-                    let memtotal=$memphy+$memswap
-                    let memtotalgb=$memtotal/100000
-                    let sscorelimit=$cores*6
-                    let ssmemlimit=$memtotalgb*6/10
-                    if [[ $sscorelimit -le $ssmemlimit ]]
-                    then
-                            number=$sscorelimit
-                    else
-                            number=$ssmemlimit
-                    fi
-                    ;;
-            "3)")
-                    export NEWT_COLORS='
-                    window=,red
-                    border=white,red
-                    textbox=white,red
-                    button=black,white
-                    '
-                    whiptail --title "WARNING" --msgbox "IF YOU SET EXCESIVE AMOUNT OF SESSIONS THIS SESSIONS MAY BE BLOCKED || RECOMMENDED USE A SINGLE SESSION" 8 78
-                    number=$(whiptail --inputbox "ENTER NUMBER OF SESSIONS" 8 78 --title "SESSIONS" 3>&1 1>&2 2>&3)
-                    numberstatus=$?
-                    if [ $numberstatus = 0 ]; then
-                            echo "All right"
-                    else
-                            echo "User selected Cancel"
-                            exit
-                            fi
+	        tokenstatus=$?
+	        if [ $tokenstatus = 0 ]; then
+	          	echo "Token has been updated to $token"
+	        else
+	           	echo "User selected cancel"
+	           	exit
+	        fi
+        option=$(whiptail --title "How many sessions you want?" --menu "Choose an option" 16 100 9 \
+		"1)" "Use one session" \
+		"2)" "Automatic max session based on system specs" \
+		"3)" "Use custom number" 3>&2 2>&1 1>&3
+		)
+		case $option in
+			"1)")
+				number=1
+				;;
+			"2)")
+				cores=`nproc --all`
+				memphy=`grep MemTotal /proc/meminfo | awk '{print $2}'`
+				memswap=`grep SwapTotal /proc/meminfo | awk '{print $2}'`
+				let memtotal=$memphy+$memswap
+				let memtotalgb=$memtotal/100000
+				let sscorelimit=$cores*6
+				let ssmemlimit=$memtotalgb*6/10
+				if [[ $sscorelimit -le $ssmemlimit ]]
+				then
+					number=$sscorelimit
+					echo "Amount of $number sessions has been set"
+				else
+					number=$ssmemlimit
+					echo "Amount of $number sessions has been set"
+				fi
+				;;
+			"3)")
+				export NEWT_COLORS='
+				window=,red
+				border=white,red
+				textbox=white,red
+				button=black,white
+				'
+				whiptail --title "WARNING" --msgbox "IF YOU SET EXCESIVE AMOUNT OF SESSIONS, SESSIONS MAY BE BLOCKED || RECOMMENDED USE A AUTOMATIC SESSION" 8 78
+				export NEWT_COLORS='
+				window=,white
+				border=black,white
+				textbox=black,white
+				button=black,white
+				'
+				number=$(whiptail --inputbox "ENTER NUMBER OF SESSIONS" 8 78 --title "SESSIONS" 3>&1 1>&2 2>&3)
+				numberstatus=$?
+				if [ $numberstatus = 0 ]; then
+					echo "Selected amount of $number sessions has been set"
+				else
+					echo "User selected Cancel"
+					exit
+				fi
                     ;;
         esac
     fi
     if [ $os == "1" ] || [ $os == "2" ]; then
         apt-get update
         apt-get upgrade -y
-        apt-get install -y unzip libcanberra-gtk-module curl libxss1 xvfb htop sed tar libxtst6 libnss3 wget
+        apt-get install -y unzip libcanberra-gtk-module curl libxss1 xvfb htop sed tar libxtst6 libnss3 wget psmisc
     else
         yum -y update
         yum install -y unzip curl xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXcomposite-0.4.4-4.1.el7.x86_64 libXScrnSaver libXcursor-1.1.15-1.el7.x86_64 libXi-1.7.9-1.el7.x86_64 libXtst-1.2.3-1.el7.x86_64 fontconfig-2.13.0-4.3.el7.x86_64 libXrandr-1.5.1-2.el7.x86_64 alsa-lib-1.1.6-2.el7.x86_64 pango-1.42.4-1.el7.x86_64 atk-2.28.1-1.el7.x86_64 psmisc
